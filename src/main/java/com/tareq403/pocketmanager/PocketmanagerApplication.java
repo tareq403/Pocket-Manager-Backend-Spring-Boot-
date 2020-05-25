d30cc1cd95b7af4966bd88f6bf14abd546a3a087
Tareq <alnoortareq4@gmail.com>
2020-05-25 14:27:38 +0600

    Initialized project with spint initialzr

---
+++ src/main/java/com/tareq403/pocketmanager/PocketmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.tareq403.pocketmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketmanagerApplication.class, args);
+    }
+
+}
