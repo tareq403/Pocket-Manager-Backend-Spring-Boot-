5ad3b2bd3145c32528206bfb13a534ccfea0de01
Tareq <alnoortareq4@gmail.com>
2020-05-27 12:42:31 +0600

    Allowed CORS

--- src/main/java/com/tareq403/pocketmanager/PocketmanagerApplication.java
+++ src/main/java/com/tareq403/pocketmanager/PocketmanagerApplication.java
@@ -2,6 +2,9 @@ package com.tareq403.pocketmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PocketmanagerApplication {
@@ -10,4 +13,15 @@ public class PocketmanagerApplication {
         SpringApplication.run(PocketmanagerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
